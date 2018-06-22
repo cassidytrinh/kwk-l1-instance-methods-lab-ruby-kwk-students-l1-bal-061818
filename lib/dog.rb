bf9f7abfe13f21867f56abdbbfeecba5ee80b197
cassidytrinh <cassidytrinh@gmail.com>
2018-06-22 19:21:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    "The Dog is sitting"
+  end
+
+end
+
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # person.rb
+class Person
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+adele_goldberg = Person.new("Adele Goldberg")
+alan_kay = Person.new("Alan Key")
